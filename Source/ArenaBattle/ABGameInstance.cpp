f3f05e64d869681ece1b045188649e512306f8c5
Jinho Kim <winda00zu@gmail.com>
2020-07-17 00:45:35 +0900

    Added StatList Data
    Excel Data

--- Source/ArenaBattle/ABGameInstance.cpp
+++ Source/ArenaBattle/ABGameInstance.cpp
@@ -3,3 +3,21 @@
 
 #include "ABGameInstance.h"
 
+UABGameInstance::UABGameInstance()
+{
+	FString CharacterDataPath = (TEXT("DataTable'/Game/Book/GameData/ABCharacterData.ABCharacterData'"));
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_ABCHARACTER(*CharacterDataPath);
+	ABCHECK(DT_ABCHARACTER.Succeeded());
+	ABCharacterTable = DT_ABCHARACTER.Object;
+	ABCHECK(ABCharacterTable->GetRowMap().Num() > 0);
+}
+
+void UABGameInstance::Init()
+{
+	Super::Init();
+}
+
+FABCharacterData* UABGameInstance::GetABCharacterData(int32 Level)
+{
+	return ABCharacterTable->FindRow<FABCharacterData>(*FString::FromInt(Level), TEXT(""));
+}
